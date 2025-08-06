eee69fa5b1fa278fac7fa33258519b4aeea496ce
20bci7179 <126901595+20bci7179@users.noreply.github.com>
2025-08-06 23:04:06 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
   public static void main(String args[]) {
-	System.out.println("Welcome to Jenkins mother f***er");
+	System.out.println("Welcome to Jenkins mother f***er using poll scm");
   }
 }
